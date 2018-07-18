c7151f2b4275afb6daa3c3ea135b018448dbcd61
uwx <cadrekucra@gmx.com>
2018-07-18 01:54:09 -0300

    downgrade to electron 1.6

--- _hansen_install_electron.cmd
+++ _hansen_install_electron.cmd
@@ -1,4 +1,4 @@
-npm i electron@2.0.2 --arch=ia32 --save
+npm i electron@1.6.18 --arch=ia32 --save
 npm install --arch=ia32
-node_modules\.bin\electron-rebuild --version 2.0.2 --arch=ia32
+node_modules\.bin\electron-rebuild --version 1.6.18 --arch=ia32
 @rem todo might have to install electron@1.6.17 electron-rebuild --version 1.6.17
