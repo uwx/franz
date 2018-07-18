45b6c68c6cedae2a2f0b30e24b810eb0f700fc70
uwx <cadrekucra@gmx.com>
2018-07-18 01:54:40 -0300

    AutoLaunch polyfill

--- src/_hansen/polyfill.js
+++ src/_hansen/polyfill.js
@@ -1,3 +1,19 @@
 export function getDoNotDisturb() {
   return false;
 }
+
+export class AutoLaunch {
+  constructor({ name }) {
+    this.name = name;
+    this.enabled = false;
+  }
+  enable() {
+    this.enabled = true;
+  }
+  disable() {
+    this.enabled = false;
+  }
+  isEnabled() {
+    return !!this.enabled;
+  }
+}
